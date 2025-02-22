cf89782224cea7539acd75e2c722e3bf697e38ee
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-02-22 23:52:58 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_map>
+
+int main() {
+    std::unordered_map<std::string, std::string> cache;
+
+    // Insert data into cache
+    cache["user_1"] = "Angad";
+    cache["user_2"] = "Bobby";
+
+    // Retrieve data from cache
+    std::cout << "user_1: " << cache["user_1"] << std::endl;
+    std::cout << "user_2: " << cache["user_2"] << std::endl;
+
+    return 0;
+}
